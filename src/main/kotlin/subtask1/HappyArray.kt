83b0deda526e4d929dcf8dcadf4cd5a7e0edef28
Vasili_Savin <vasili_savin@epam.com>
2021-05-21 18:02:27 +0300

    add implementation of tasks

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,23 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val happyList: MutableList<Int> = mutableListOf()
+
+        for ((index, value) in sadArray.withIndex()){
+            if (index == 0 || index == sadArray.size - 1){
+                happyList.add(value)
+                continue
+            }
+            val sum = sadArray[index - 1] + sadArray[index + 1]
+            if (sum >= value){
+                happyList.add(value)
+            }
+
+        }
+
+        val happyArray = happyList.toIntArray()
+
+        return if (happyArray contentEquals  sadArray) happyArray else convertToHappy(happyArray)
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,29 @@
 package subtask3
 
 class StringParser {
+    private val bracketsMap = mapOf('[' to ']', '(' to ')', '<' to '>')
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val resultList: MutableList<String> = mutableListOf();
+        var currentString = inputString
+        while (currentString.isNotEmpty()){
+            val startChar = currentString[0]
+            if (bracketsMap.containsKey(startChar)){
+                val closeChar = bracketsMap[startChar]
+                var score = 0
+                for ((index, value) in currentString.withIndex()){
+                    when (value){
+                        startChar -> score++
+                        closeChar -> score--
+                    }
+                    if (score == 0) {
+                        resultList.add(currentString.substring(1, index))
+                        break
+                    }
+                }
+            }
+            currentString = currentString.substring(1)
+        }
+        return resultList.toTypedArray()
     }
 }
